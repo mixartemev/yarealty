e432f554f06ae97a690f9e992ae942a47a96518e
mix <mixartemev@gmail.com>
2019-04-24 13:50:47 +0300

    update

--- sheet/update.py
+++ sheet/update.py
@@ -18,6 +18,37 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1lPFc1p_5TNSxYOtJ4hSqcSMAiUig4slRQTdMmgJroic'
 
 
+def to_sheet(offers: list):
+    body = {'values': []}
+    for o in offers:
+        body['values'].append([
+            o.id,
+            o.cianUserId if o.cianUserId else '=HYPERLINK("https://www.mcity.ru/{}";"{}")'.format(o.idd, o.idd) if o.idd else None,
+            o.bc.name if o.bc_id else None,
+            o.house.name if o.house_id else None,
+            o.newbuilding.name if o.newbuilding_id else None,
+            o.description,
+            str(o.creationDate),
+            str(o.editDate),
+            str(o.publishDate),
+            o.category,
+            o.dealType,
+            o.status,
+            o.bargainTerms_currency,
+            o.price,
+            o.pricePerUnitArea,
+            o.floorNumber,
+            str(o.totalArea),
+            o.services,
+            o.userTrust,
+            o.isPro,
+            o.stats_total,
+            o.stats_daily,
+            o.publishTerms_autoprolong,
+        ])
+    return body
+
+
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
@@ -47,39 +78,15 @@ def main():
     # spreadsheet = service.spreadsheets().create(body=spreadsheet, fields='spreadsheetId').execute()
     # print('Spreadsheet ID: {0}'.format(spreadsheet.get('spreadsheetId')))
 
-    offers = session.query(RivalOffer).all()
-    body = {'values': []}
-
-    o: RivalOffer
-    for o in offers:
-        body['values'].append([
-            o.id,
-            o.cianUserId if o.cianUserId else '=HYPERLINK("https://www.mcity.ru/{}";"{}")'.format(o.idd, o.idd) if o.idd else None,
-            o.bc.name if o.bc_id else None,
-            o.house.name if o.house_id else None,
-            o.newbuilding.name if o.newbuilding_id else None,
-            o.description,
-            str(o.creationDate),
-            str(o.editDate),
-            str(o.publishDate),
-            o.category,
-            o.dealType,
-            o.status,
-            o.bargainTerms_currency,
-            o.price,
-            o.pricePerUnitArea,
-            o.floorNumber,
-            str(o.totalArea),
-            o.services,
-            o.userTrust,
-            o.isPro,
-            o.stats_total,
-            o.stats_daily,
-            o.publishTerms_autoprolong,
-        ])
+    offers = session.query(Offer).all()
+    rival_offers = session.query(RivalOffer).all()
 
     result = service.spreadsheets().values().update(
-        spreadsheetId=SPREADSHEET_ID, range='rival!A2', valueInputOption='USER_ENTERED', body=body
+        spreadsheetId=SPREADSHEET_ID, range='mcity!A2', valueInputOption='USER_ENTERED', body=to_sheet(offers)
+    ).execute()
+    pprint(result)
+    result = service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID, range='rival!A2', valueInputOption='USER_ENTERED', body=to_sheet(rival_offers)
     ).execute()
     pprint(result)
 
