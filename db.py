8d59fecab6d3fbc30363ba7a777984f353213049
artemiev <mixartemev@gmail.com>
2019-07-14 23:30:28 +0300

    mysql-connector

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from models import Base
 
 # Create connection
 # engine = create_engine('postgresql://mixartemev:mixfixX98@95.163.209.94/Postgres-mc', echo=False)
-engine = create_engine('mysql://mixartemev:mixfixX98@old.mcity.ru/cian', echo=False)
+engine = create_engine('mysql+mysqlconnector://mixartemev:mixfixX98@old.mcity.ru/cian', echo=False)
 # Base.metadata.schema = 'cian'
 # Create all not created defined tables
 Base.metadata.create_all(engine)
